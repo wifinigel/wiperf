defc516d16fbdb11707395fc72daef3fe3fe0cfd
Nigel Bowden <wifinigel@gmail.com>
2020-01-23 19:12:41 +0000

    increased file archive to 10

--- modules/filelogger.py
+++ modules/filelogger.py
@@ -7,6 +7,7 @@ import logging
 import time
 from logging.handlers import RotatingFileHandler
 
+
 def FileLogger(log_file):
     '''
     A class to perform very simple logging to a named file. Any non-recoverable
@@ -15,14 +16,13 @@ def FileLogger(log_file):
 
     logger = logging.getLogger("Probe_Log")
     logger.setLevel(logging.INFO)
- 
+
     # add a rotating handler
-    handler = RotatingFileHandler(log_file, maxBytes=521000, backupCount=5)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    
+    handler = RotatingFileHandler(log_file, maxBytes=521000, backupCount=10)
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
     handler.setFormatter(formatter)
     logger.addHandler(handler)
-    
-    return logger
-    
 
+    return logger
