6b0647100fb427ba568c70d1c0ea724a2571d2b3
Nigel Bowden <wifinigel@gmail.com>
2020-02-27 15:41:11 +0000

    no longer required

--- post_install.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-#
-# Copy files we want to preserve after upgrade to the live dir
-cp /tmp/wiperf/config.ini /home/wlanpi/wiperf
-cp /tmp/wiperf/conf/etc/wpa_supplicant/wpa_supplicant.conf /home/wlanpi/wiperf/conf/etc/wpa_supplicant/wpa_supplicant.conf
