ba1b401a949723129ba0d1c3117cd76a7d70b6cb
Nigel Bowden <wifinigel@gmail.com>
2020-06-16 16:19:13 +0100

    clear out wiperf repo if already exists locally

--- install.sh
+++ install.sh
@@ -75,6 +75,8 @@ install () {
 
   ### Pull in the wiperf github code
   echo "(ok) Cloning GitHub wiperf repo (please wait)..." | tee -a $LOG_FILE
+  # get rid of the local copy if already exists (in case installing over the top)
+  rm -rf $CLONE_DIR >> $LOG_FILE 2>&1
   git -C $CLONE_DIR clone $GITHUB_REPO -b $GITHUB_BRANCH >> $LOG_FILE 2>&1
   if [ "$?" != '0' ]; then
     echo "(fail) Clone of GitHub repo failed." | tee -a $LOG_FILE
