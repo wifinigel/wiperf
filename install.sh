14dad1614a5f62c20e4ab032064e65ea86c6211c
Nigel Bowden <wifinigel@gmail.com>
2020-06-16 15:44:15 +0100

    added rpi support

--- install.sh
+++ install.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
-# Installer for wiperf on WLAN Pi
+# Installer for wiperf on WLAN Pi & RPi
 
 # Installation script log file
 LOG_FILE="/var/log/wiperf_install.log"
 
-# install dir
+# Check which platform we're installing for
+read -p 'Enter your platform type (wlanpi/rpi): ' PLATFORM
+if ! [[ $PLATFORM =~ ^(wlanpi|rpi)$ ]]; then
+  echo "Unknown platform (exiting)"
+  exit 1
+fi
+
+# define global vars
 CLONE_DIR="/usr/share"
 INSTALL_DIR="$CLONE_DIR/wiperf"
 CFG_DIR="/etc/wiperf"
-
 GITHUB_REPO="https://github.com/wifinigel/wiperf.git"
 GITHUB_BRANCH='conf_pull'
 
+# install function
 install () {
 
   echo "(ok) Starting wiperf install process (see $LOG_FILE for details)" | tee $LOG_FILE 
 
-  # check we can get to pypi
+  ### check we can get to pypi before staring
   curl -s --head  -m 2 --connect-timeout 2 --request GET https://pypi.org | head -n 1 | grep '200'  >> $LOG_FILE 2>&1
   if [ "$?" != '0' ]; then
     echo "Unable to reach Internet - check connection (exiting)" | tee -a $LOG_FILE 
     exit 1
   fi
 
-  # check git is present
+  ### check git is present
   echo "(ok) Checking we have git available..."
   `git --version`  >> $LOG_FILE 2>&1
   if [ "$?" != '0' ]; then
@@ -33,7 +40,7 @@ install () {
     echo "(ok) Git looks OK"  | tee -a $LOG_FILE
   fi
 
-  # install the wiperf poller from PyPi - exit if errors
+  ### install the wiperf poller from PyPi - exit if errors
   echo "(ok) Installing wiperf python module (please wait)..."  | tee -a $LOG_FILE
   pip3 install wiperf_poller >> $LOG_FILE 2>&1
   if [ "$?" != '0' ]; then
@@ -43,12 +50,10 @@ install () {
       echo "(ok) wiperf_poller module python installed" | tee -a $LOG_FILE 
   fi
 
-  # pull & install the Splunk Event collector class
+  ### pull & install the Splunk Event collector class
   echo "(ok) Cloning the Splunk Event collector class..." | tee -a $LOG_FILE
-
   # take out existing dir (if there)
   rm -rf /tmp/Splunk-Class-httpevent
-
   git -C /tmp clone https://github.com/georgestarcher/Splunk-Class-httpevent.git >> $LOG_FILE 2>&1
 
   if [ "$?" != '0' ]; then
@@ -58,7 +63,7 @@ install () {
     echo "(ok) Python Splunk module cloned OK." | tee -a $LOG_FILE
   fi 
 
-  # Install the module
+  ### Install the Splunk collector module
   echo "(ok) Installing the Splunk Event collector class (please wait)..." | tee -a $LOG_FILE
   pip3 install /tmp/Splunk-Class-httpevent >> $LOG_FILE 2>&1
   if [ -z "$?" ]; then
@@ -68,7 +73,7 @@ install () {
     echo "(ok) Splunk Python module installed OK." | tee -a $LOG_FILE
   fi
 
-  # Pull in the wiperf github code
+  ### Pull in the wiperf github code
   echo "(ok) Cloning GitHub wiperf repo (please wait)..." | tee -a $LOG_FILE
   git -C $CLONE_DIR clone $GITHUB_REPO -b $GITHUB_BRANCH >> $LOG_FILE 2>&1
   if [ "$?" != '0' ]; then
@@ -78,7 +83,7 @@ install () {
     echo "(ok) Cloned OK." | tee -a $LOG_FILE
   fi
 
-  # copy config.ini.default to $CFG_DIR
+  ### copy config.ini.default to $CFG_DIR
   echo "(ok) Moving config.default.ini to $CFG_DIR..." | tee -a $LOG_FILE
   mkdir -p $CFG_DIR  >> $LOG_FILE 2>&1
   mv "$INSTALL_DIR/config.default.ini" $CFG_DIR  >> $LOG_FILE 2>&1
@@ -89,42 +94,71 @@ install () {
     echo "(ok) Copied OK." | tee -a $LOG_FILE
   fi
 
-  # move files in ./conf to $CFG_DIR
-  echo "(ok) Moving conf directory to $CFG_DIR..." | tee -a $LOG_FILE
-  mv "$INSTALL_DIR/conf" $CFG_DIR  >> $LOG_FILE 2>&1
-  if [ -z "$?" ]; then
-    echo "(fail) Copy of conf directory failed." | tee -a $LOG_FILE
-    exit 1
+  ### move files in ./conf to $CFG_DIR for wlanpi, remove dir for rpi
+  if [ "$PLATFORM" = 'wlanpi' ]; then
+    echo "(ok) Moving conf directory to $CFG_DIR..." | tee -a $LOG_FILE
+    mv "$INSTALL_DIR/conf" $CFG_DIR  >> $LOG_FILE 2>&1
+      if [ -z "$?" ]; then
+        echo "(fail) Copy of conf directory failed." | tee -a $LOG_FILE
+        exit 1
+      else
+        echo "(ok) Copied OK." | tee -a $LOG_FILE
+      fi
   else
-    echo "(ok) Copied OK." | tee -a $LOG_FILE
-  fi
+    # remove the conf dir if rpi, as don't need it
+    echo "(ok) Removing conf directory $CFG_DIR...(not needed on RPi)" | tee -a $LOG_FILE
+    rm -rf $CFG_DIR >> $LOG_FILE 2>&1
+  fi 
 
-  # copy wiperf_switcher to /usr/bin/wiperf_switcher
-  echo "(ok) Moving wiperf_switcher to /usr/bin/wiperf_switcher..." | tee -a $LOG_FILE
-  mv "$INSTALL_DIR/wiperf_switcher" /usr/bin/wiperf_switcher  >> $LOG_FILE 2>&1
+  ### copy across the wiperf switcher if this is a WLAN Pi, remove if rpi 
+  if [ "$PLATFORM" = 'wlanpi' ]; then
+    # copy wiperf_switcher to /usr/bin/wiperf_switcher
+    echo "(ok) Moving wiperf_switcher to /usr/bin/wiperf_switcher..." | tee -a $LOG_FILE
+    mv "$INSTALL_DIR/wiperf_switcher" /usr/bin/wiperf_switcher  >> $LOG_FILE 2>&1
 
-  if [ "$?" != '0' ]; then
-    echo "(fail) Copy of wiperf_switcher failed." | tee -a $LOG_FILE
-    exit 1
+    if [ "$?" != '0' ]; then
+      echo "(fail) Copy of wiperf_switcher failed." | tee -a $LOG_FILE
+      exit 1
+    else
+      echo "(ok) Copied OK." | tee -a $LOG_FILE
+      # make sure it can be executed
+      chmod 755 /usr/bin/wiperf_switcher
+    fi
   else
-    echo "(ok) Copied OK." | tee -a $LOG_FILE
-    # make sure it can be executed
-    chmod 755 /usr/bin/wiperf_switcher
+      # remove the conf dir if rpi, as don't need it
+      echo "(ok) Removing wiperf_switcher file...(not needed on RPi)" | tee -a $LOG_FILE
+      rm -f $INSTALL_DIR/wiperf_switcher >> $LOG_FILE 2>&1
   fi
 
   echo "(ok) Install complete." | tee -a $LOG_FILE
 
-  echo ""
-  echo "================================================="
-  echo "Don'tforget to modify the following files before"
-  echo "switching in to wiperf mode:"
-  echo ""
-  echo " 1. Edit $CFG_DIR/conf/etc/wpa_supplicant/wpa_supplicant.conf" 
-  echo "    (add WLAN info)"
-  echo " 2. Copy $CFG_DIR/config.default.ini to $CFG_DIR/config.ini"
-  echo " 3. Edit $CFG_DIR/config.ini for your env"
-  echo "================================================="
-  echo ""
+  if [ "$PLATFORM" = 'wlanpi' ]; then
+    echo ""
+    echo "================================================="
+    echo "Don'tforget to modify the following files before"
+    echo "switching in to wiperf mode:"
+    echo ""
+    echo " 1. Edit $CFG_DIR/conf/etc/wpa_supplicant/wpa_supplicant.conf" 
+    echo "    (add WLAN info)"
+    echo " 2. Copy $CFG_DIR/config.default.ini to $CFG_DIR/config.ini"
+    echo " 3. Edit $CFG_DIR/config.ini for your env"
+    echo "================================================="
+    echo ""
+  else
+    echo ""
+    echo "================================================="
+    echo "Don'tforget to modify the following files before"
+    echo "to use wiperf:"
+    echo ""
+    echo " 1. Edit /etc/wpa_supplicant/wpa_supplicant.conf (add WLAN info)"
+    echo " 2. Edit /etc/network/interfaces (edit wlan0 config)" 
+    echo " 3. Copy $CFG_DIR/config.default.ini to $CFG_DIR/config.ini"
+    echo " 4. Edit $CFG_DIR/config.ini for your env"
+    echo " 5. Add a cron job to run wiperf regularly, e.g."
+    echo "    0-59/5 * * * * /usr/bin/python3 /usr/share/wiperf/wiperf_run.py > /var/log/wiperf_cron.log 2>&1"
+    echo "================================================="
+    echo ""
+  fi
 }
 
 uninstall () {
@@ -157,6 +191,16 @@ case "$1" in
         ;;
   -i)
         install
+        ;;
+  -h)
+        echo "Usage: install.sh {-i | -u | -h}"
+        echo ""
+        echo "  install.sh    : run installer (default)"
+        echo "  install.sh -h : show this help message"
+        echo "  install.sh -u : unistall wiperf completely"
+        echo "  install.sh -i : install (default action if run with no args)"
+        echo ""
+        exit 0
         ;;
   *)
         install
