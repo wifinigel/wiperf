ed1ec54e568764515e203d99053a71d5950893c7
Nigel Bowden <wifinigel@gmail.com>
2020-08-21 19:37:14 +0100

    Moved to 2.0.0 beta5

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Installer for wiperf on WLAN Pi & RPi
-VERSION='2.0.0-beta 4'
-POLLER_VERSION='0.1.22'
+VERSION='2.0.0-beta 5'
+POLLER_VERSION='0.1.24'
 
 # Installation script log file
 LOG_FILE="/var/log/wiperf_install.log"
