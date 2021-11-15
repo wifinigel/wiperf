640a7a2c3c9427b8e014629e564bd75756a28357
Wifinigel <wifinigel@gmail.com>
2021-11-15 21:30:12 +0000

    Move to v2.2.1 (influx ssl fix)

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Installer for wiperf on WLAN Pi & RPi
-VERSION='2.2.0'
-POLLER_VERSION='0.3.0'
+VERSION='2.2.1'
+POLLER_VERSION='0.3.2'
 
 # Installation script log file
 LOG_FILE="/var/log/wiperf_install.log"
